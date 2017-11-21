d778b2575459eae7ddd9790aaed07d1769e44992
Dmytro <dima.golysh@gmail.com>
2017-11-21 15:50:23 +0200

    - added basic structure; (#2)
    - added course-details interface;
    - set up webpack config for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { CourseDetailsComponent } from './pages/courses/course-details/course-details.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { SearchComponent } from './common/search/search.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
