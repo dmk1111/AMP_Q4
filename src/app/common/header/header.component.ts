da5ccf471195f76b727f2fb5f0dfec305962129a
Dmytro Golysh <dima.golysh@gmail.com>
2018-01-28 12:41:40 +0200

    added integration for Auth Service

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,22 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import {AuthorizationService} from '../../services/authorization.service';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   public placeholder = 'user login';
   public username: string;
   authorized: boolean;
 
-  constructor(public authServ: AuthorizationService) {
+  private subscription: Subscription;
+
+  constructor(private authServ: AuthorizationService) {
   }
 
   ngOnInit() {
     this.isAuthorized();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   logOff() {
@@ -25,11 +32,15 @@ export class HeaderComponent implements OnInit {
   }
 
   isAuthorized(): void {
-    this.authServ.isAuth()
+    this.subscription = this.authServ.isAuth()
       .subscribe(auth => {
         this.username = this.authServ.getUserInfo();
         this.authorized = auth;
-      });
+      },
+        err => {
+        this.username = "";
+        this.authorized = false
+        });
   }
 
 }
--- src/app/services/authorization.service.ts
+++ src/app/services/authorization.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Injectable } from "@angular/core";
+import { ReplaySubject } from "rxjs/ReplaySubject";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import "rxjs/add/operator/debounceTime";
 
 @Injectable()
 export class AuthorizationService {
 
   private auth: ReplaySubject<boolean>;
+  private baseUrl: string;
+  private userInfo: any = {};
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.auth = new ReplaySubject<boolean>();
+    this.baseUrl = "http://localhost:3004";
   }
 
-  logIn(username: string, token: string): void {
-    localStorage.setItem('username', username);
-    localStorage.setItem('token', token);
-    this.auth.next(true);
+  logIn(username: string, password: string): void {
+    let body = {
+      login: username,
+      password: password
+    };
+    let url = `${this.baseUrl}/auth/login`;
+
+    this.http.post(url, body)
+      .subscribe((data: { token: string }) => {
+          localStorage.setItem("username", username);
+          localStorage.setItem("token", data.token);
+          this.auth.next(true);
+        },
+        err => {
+          this.auth.next(false);
+        });
   }
 
   logOut(): void {
@@ -22,15 +39,33 @@ export class AuthorizationService {
   }
 
   isAuth(): ReplaySubject<boolean> {
+    let body = {
+      login: localStorage.getItem("username")
+    };
+    let url = `${this.baseUrl}/auth/userinfo`;
+    let options = {
+      headers: new HttpHeaders({'Authorization':`${localStorage.getItem("token")}`}),
+    };
+
+    this.http.post(url, body, options)
+      .debounceTime(500)
+      .subscribe((data) => {
+          this.userInfo = data;
+          this.auth.next(true);
+        },
+        err => {
+          this.userInfo = {};
+          this.auth.next(false);
+        });
     return this.auth;
   }
 
   checkAuth(): void {
-    this.auth.next(!!localStorage.getItem('username'));
+    this.auth.next(!!localStorage.getItem("username"));
   }
 
   getUserInfo(): string {
-    return localStorage.getItem('username');
+    return this.userInfo.login;
   }
 
 }
