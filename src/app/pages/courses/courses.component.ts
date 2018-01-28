041c7d525315f28336112338ba366cc4ee8a035c
Dmytro Golysh <dima.golysh@gmail.com>
2018-01-28 10:27:50 +0200

    unsubscribe from Observables

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ICourseDetails } from './course-details/course-details.interface';
 import { CoursesService } from '../../services/courses.service';
 import { OrderByPipe } from '../../pipes/order-by.pipe';
 import { SearchPipe } from '../../pipes/search.pipe';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-courses',
@@ -10,19 +11,19 @@ import { SearchPipe } from '../../pipes/search.pipe';
   styleUrls: ['./courses.component.css'],
   providers: [OrderByPipe, SearchPipe]
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements OnInit, OnDestroy {
 
   public courses: ICourseDetails[] = [];
   public editing: boolean = false;
 
   private searchValue = '';
+  private subscription: Subscription;
 
   constructor(private courseServ: CoursesService, private searchPipe: SearchPipe) { }
 
   ngOnInit() {
-    this.courseServ.getList()
+    this.subscription = this.courseServ.getList()
       .map( item => {
-        console.log(item);
         if (!item.courseDate) {
             item.courseDate = new Date().toISOString();
         }
@@ -31,19 +32,21 @@ export class CoursesComponent implements OnInit {
       .subscribe( courses => this.courses.push(courses));
     this.courseServ.isEditingCourse()
       .subscribe( editing => {
-        console.log(editing);
         this.editing = editing;
       });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   removeCourse(item) {
-    console.log(`Course deleted!`);
     this.courseServ.removeItem(item);
+    this.subscription.unsubscribe();
     if (!this.searchValue) {
       this.courses = [];
-      this.courseServ.getList()
+      this.subscription = this.courseServ.getList()
         .map( item => {
-          console.log(item);
           if (!item.courseDate) {
             item.courseDate = new Date().toISOString();
           }
@@ -58,9 +61,8 @@ export class CoursesComponent implements OnInit {
   search(value: string): any {
     let coursesArr = [];
     this.searchValue = value;
-    this.courseServ.getList()
+    this.subscription = this.courseServ.getList()
       .map( item => {
-        console.log(item);
         if (!item.courseDate) {
           item.courseDate = new Date().toISOString();
         }
