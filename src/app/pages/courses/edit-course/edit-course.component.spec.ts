a7fb43ecca405341145478f118c16bd6082edc1c
dmk1111 <dima.golysh@gmail.com>
2018-10-01 13:36:23 +0300

    test: fix unit tests, `karma` major update

--- src/app/pages/courses/edit-course/edit-course.component.spec.ts
+++ src/app/pages/courses/edit-course/edit-course.component.spec.ts
@@ -1,6 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditCourseComponent } from './edit-course.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DurationPipe} from '../../../pipes/duration.pipe';
+import {SearchPipe} from '../../../pipes/search.pipe';
+import {NgModule} from '@angular/core';
+import {OrderByPipe} from '../../../pipes/order-by.pipe';
+import {AuthorsComponent} from '../../../common/controls/authors/authors.component';
+import {HttpClientModule} from '@angular/common/http';
+import {CoursesService} from '../../../services/courses.service';
+import {RouterTestingModule} from '@angular/router/testing';
+
+@NgModule({
+  imports: [HttpClientModule, RouterTestingModule],
+  declarations: [
+    DurationPipe,
+    OrderByPipe,
+    SearchPipe,
+  ],
+  providers: [
+    DurationPipe,
+    CoursesService
+  ],
+  exports: [
+    DurationPipe,
+    OrderByPipe,
+    SearchPipe]
+})
+
+export class ReusedFeatureModule { }
 
 describe('EditCourseComponent', () => {
   let component: EditCourseComponent;
@@ -8,7 +37,13 @@ describe('EditCourseComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditCourseComponent ]
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ReusedFeatureModule
+      ],
+      declarations: [ EditCourseComponent, AuthorsComponent ],
     })
     .compileComponents();
   }));
